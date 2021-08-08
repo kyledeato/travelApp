b6ba9db345fcd9d489d753b8e948f195c6801ee7
Kyle Deato <kyledeato@gmail.com>
2021-08-08 16:53:09 -0700

    fixed fetch

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-//Foursquare API Info
 
 //OpenWeather Info
 const weatherKey = '82510feaa0c1d3a300a7a754ff134404';
 const weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
 
-
-
 //Page Elements
-const input = document.querySelector('#city');
-const button = document.querySelector('#button');
+let input = document.querySelector("#input");
+const button = document.querySelector('#submit');
 const destination = document.querySelector('#destination');
-const weatherDiv = document.querySelector('#weather1');
+
 //Fetch
-const getForecast = async () => {
-    const urlToFetch = `${weatherURL}?&q=${input.value}&APPID=${weatherKey}`;
+const getWeather = async () => {
     try {
-        const response = await fetch(urlToFetch);
+        console.log(input.value);
+        const apiURL = `${weatherURL}?&q=${input.value}&APPID=${weatherKey}`;
+        console.log(apiURL);
+        const response = await fetch(apiURL);
         if (response.ok) {
             const jsonResponse = await response.json();
-            console.log(jsonResponse)
+            console.log(jsonResponse);
             return jsonResponse;
+        } else {
+            console.log("request failed!");
         }
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
-//Populate
-function populate(data) {
-    weatherDiv.innerHTML = ` 
-    <h2>${data.name}</h2>`
+
+//K to F
+const kToF = (kelvin) => {
+    return Math.round((kelvin - 273) * (9/5) + 32);
+}
+const renderWeather = (data) => {
+    document.querySelector("#weather-degrees").innerHTML = kToF(data.main.temp) + "°F";
+    document.querySelector("#feels-like").innerHTML = "Feels like: " + kToF(data.main.temp) + "°F";
+    document.querySelector("#icon").src = "http://openweathermap.org/img/wn/" + data.weather[0].icon +".png";
+    document.querySelector("#overview").innerHTML = data.weather[0].description;
+    document.querySelector("#humidity").innerHTML = kToF(data.main.temp) + "°F";
 }
 
-//Render Functions
-function render(input) {
-    getForecast().then(data => {
-        populate(data);
-    })
+const executeSearch = () => {
+    event.preventDefault()
+    getWeather().then(data => renderWeather(data));
 }
 
-button.addEventListener('click', () => {
-    const currentVal = input.value;
-
-    getForecast().then(data => {
-        populate(data)
-    })
-    
-})
+button.addEventListener('click', executeSearch);
